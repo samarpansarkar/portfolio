5ff55b0f4397351acefccf00f5303a6a1cd988b8
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-12-03 17:49:30 +0530

    Update skills.jsx

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -59,7 +59,23 @@ const skills = () => {
                 <div className='flex justify-center items-center'>
                     <div className='xl:grid xl:grid-cols-3 xl:gap-9  gap-4 mt-2'>
                         {projectData.map((item) => (
-                            <ProjectList name={item.name} image={item.image} liveLink={item.liveLink} githubLink={item.githubLink} stack={item.stack} />
+                            // <ProjectList name={item.name} image={item.image} liveLink={item.liveLink} githubLink={item.githubLink} stack={item.stack} />
+                            <div className="bg-gray-100 p-3 rounded-lg group overflow-hidden cursor-pointer relative z-50 hover:before:bg-black before:absolute before:inset-0 before:opacity-20 before:transition-all">
+                                <div className="w-full h-[200px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                                    <img src={item.image} alt={item.name}
+                                        className="h-full w-full object-contain" />
+                                </div>
+                                <div className="absolute mx-auto left-0 right-0 -bottom-80 group-hover:bottom-2 bg-white w-11/12 p-3 rounded-lg transition-all duration-300">
+                                    <div className="text-center">
+                                        <h3 className="font-bold text-blue-600 text-2xl">{item.name}</h3>
+                                        <h4 className="text-lg text-gray-600 font-bold mt-2">{item.stack}</h4>
+                                        <div className="border py-3 px-2">
+                                            <a href={item.liveLink} target="_blank" className="inline-block text-white font-semibold mr-2 bg-[#833be7cb] p-2 rounded-md">Live Demo</a>
+                                            <a href={item.githubLink} target="_blank" className="inline-block text-white font-semibold mr-2 bg-[#833be7cb] p-2 rounded-md">github repo</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>

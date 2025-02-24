99a376b96848530b2c0969e3b8021ebee6c71f2e
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2025-02-24 22:56:40 +0530

    Fixing All Bugs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
 import Content from "./components/content";
 import Navbar from "./components/navbar";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/about";
-import Skills from "./components/skills"
+import Skills from "./components/skills";
 import Contact from "./components/contact";
-import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
@@ -17,15 +15,19 @@ function App() {
           <Route path='/about' Component={About} />
           <Route path='/skills&projects' Component={Skills} />
           <Route path='/contact' Component={Contact} />
-          <Route path='/login' Component={LoginPage} />
           <Route path='*' component={() => <h1>404 Not Found</h1>} />
         </Routes>
       </BrowserRouter>
-      <div className="flex justify-center">
-        <h1 className="flex">made by <span className="font-bold ml-2 uppercase cursor-pointer text-stone-600 hover:text-purple-600"><a href="https://github.com/samarpansarkar" target='_blank'>samarpan sarkar</a></span></h1>
+      <div className='flex justify-center'>
+        <h1 className='flex'>
+          made by{" "}
+          <span className='font-bold ml-2 uppercase cursor-pointer text-stone-600 hover:text-purple-600'>
+            <a href='https://github.com/samarpansarkar' target='_blank'>
+              samarpan sarkar
+            </a>
+          </span>
+        </h1>
       </div>
-
-
     </div>
   );
 }
